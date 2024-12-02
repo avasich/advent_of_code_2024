7e35569379a1b7321ea0fe96c55c8478b5cee765
Artyom Vasich <vasich.dev@gmail.com>
2024-12-02 23:39:42 +0400

    day 02 p2

--- src/days/d02_red_nosed_reports.rs
+++ src/days/d02_red_nosed_reports.rs
@@ -1,35 +1,63 @@
-#![allow(non_contiguous_range_endpoints)]
+use Kind::*;
 
 use crate::utils::{Day, Task, read_lines};
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Kind {
-    Inc,
     Dec,
+    Inc,
     Unsafe,
 }
 
-fn p1_count_safe(filename: &str) -> usize {
-    use Kind::*;
+impl Kind {
+    fn new(a: i32, b: i32) -> Self {
+        Self::from_diff(b - a)
+    }
 
+    fn from_diff(diff: i32) -> Self {
+        #[allow(non_contiguous_range_endpoints)]
+        match diff {
+            -3..0 => Dec,
+            1..=3 => Inc,
+            _ => Unsafe,
+        }
+    }
+}
+
+fn p1_count_safe(filename: &str) -> usize {
     read_lines(filename)
-        .map(|line| {
-            line.split_whitespace()
-                .flat_map(str::parse::<i32>)
-                .map_windows(|[a, b]| match b - a {
-                    -3..0 => Dec,
-                    1..=3 => Inc,
-                    _ => Unsafe,
-                })
-                .map_windows(|[s1, s2]| matches!([s1, s2], [Inc, Inc] | [Dec, Dec]))
-                .all(|x| x)
-        })
+        .map(|line| find_unsafe(line.split_whitespace().flat_map(str::parse::<i32>)).is_none())
         .filter(|&x| x)
         .count()
 }
 
-fn p2(filename: &str) -> usize {
-    todo!()
+fn p2_count_safe_allow_error(filename: &str) -> usize {
+    read_lines(filename)
+        .map(|line| line.split_whitespace().flat_map(str::parse::<i32>).collect::<Vec<_>>())
+        .map(|v| with_error_allowed(&v, None))
+        .filter(|&x| x)
+        .count()
+}
+
+fn find_unsafe(xs: impl Iterator<Item = i32>) -> Option<[usize; 3]> {
+    xs.map_windows(|&[a, b]| Kind::new(a, b))
+        .map_windows(|[s1, s2]| !matches!([s1, s2], [Inc, Inc] | [Dec, Dec]))
+        .enumerate()
+        .find_map(|(i, err)| err.then_some([i, i + 1, i + 2]))
+}
+
+fn with_error_allowed(xs: &[i32], skip: Option<usize>) -> bool {
+    match skip {
+        Some(skip) => {
+            let start = skip.saturating_sub(2);
+            let iter = std::iter::chain(&xs[start..skip], &xs[(skip + 1)..]).copied();
+            find_unsafe(iter).is_none()
+        }
+        None => match find_unsafe(xs.iter().copied()) {
+            None => true,
+            Some(js) => js.iter().any(|&j| with_error_allowed(xs, Some(j))),
+        },
+    }
 }
 
 pub const SOLUTION: Day<usize, usize> = Day {
@@ -41,9 +69,17 @@ pub const SOLUTION: Day<usize, usize> = Day {
     part_2: Task {
         examples: &["./inputs/day_02/example.txt"],
         task: "./inputs/day_02/task.txt",
-        func: p2,
+        func: p2_count_safe_allow_error,
     },
 };
+
+#[cfg(test)]
+mod d02_playground {
+
+    #[test]
+    #[ignore]
+    fn playground() {}
+}
 
 #[cfg(test)]
 mod d02_tests {
@@ -54,4 +90,18 @@ mod d02_tests {
         let res = SOLUTION.part_1.run_example(0);
         assert_eq!(res, 2);
     }
+
+    #[test]
+    fn p2_example_test() {
+        let res = SOLUTION.part_2.run_example(0);
+        assert_eq!(res, 4);
+    }
 }
+
+/*
+0  100  -1   2
+  inc 100     dec -101     inc 3
+
+0  100  -1   101
+  inc 100     dec -101     inc 102
+ */
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(iter_map_windows)]
 #![feature(iterator_try_reduce)]
+#![feature(iter_chain)]
 
 pub mod utils;
 pub mod days;
