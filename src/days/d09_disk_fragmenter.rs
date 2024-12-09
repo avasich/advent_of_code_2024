0c478d35c0ddb23b90a608d1f01141db92fcf417
Artyom Vasich <vasich.dev@gmail.com>
2024-12-09 23:56:51 +0400

    day 09 p2

--- src/days/d09_disk_fragmenter.rs
+++ src/days/d09_disk_fragmenter.rs
@@ -1,21 +1,13 @@
+use std::collections::BTreeSet;
+
 use itertools::Itertools;
 
 use crate::utils::{Day, Task, read_lines};
 
-fn _print_arr(arr: &[Option<usize>]) {
-    let s = arr.iter().fold(String::new(), |mut s, c| {
-        let a = c.map(|c| c.to_string()).unwrap_or(".".to_string());
-        s.push_str(&a);
-        s.push(' ');
-        s
-    });
-    println!("{s}");
-}
-
 fn p1(filename: &str) -> usize {
-    let line = read_lines(filename).next().unwrap();
-
-    let mut arr = line
+    let mut arr = read_lines(filename)
+        .next()
+        .unwrap()
         .chars()
         .flat_map(|c| c.to_digit(10))
         .enumerate()
@@ -33,7 +25,7 @@ fn p1(filename: &str) -> usize {
         rt -= arr[..=rt].iter().rev().take_while(|x| x.is_none()).count();
 
         if lt >= rt {
-            return arr.iter().enumerate().filter_map(|(i, x)| x.map(|x| x * i)).sum();
+            return arr.iter().flatten().enumerate().map(|(i, &x)| i * x).sum();
         }
 
         arr.swap(lt, rt);
@@ -41,18 +33,73 @@ fn p1(filename: &str) -> usize {
         rt -= 1;
     }
 }
-fn p2(filename: &str) -> u64 {
-    0
+
+// len id
+// type Block = (usize, usize);
+
+// start len id
+type Files = BTreeSet<(usize, usize, usize)>;
+// len - [positions]
+type EmptyBlocks = Vec<BTreeSet<usize>>;
+
+fn p2_(line: &str) -> usize {
+    let (mut files, mut empty, _) =
+        line.chars().flat_map(|c| c.to_digit(10)).map(|c| c as usize).enumerate().fold(
+            (BTreeSet::new(), vec![BTreeSet::new(); 10], 0),
+            |(mut files, mut empty, pos): (Files, EmptyBlocks, _), (i, len)| {
+                match i % 2 == 0 {
+                    true => files.insert((pos, len, i / 2)),
+                    false => empty[len].insert(pos),
+                };
+
+                (files, empty, pos + len)
+            },
+        );
+
+    let mut res = 0;
+
+    while let Some(f) = files.iter().last().copied() {
+        let (file_pos, file_len, id) = f;
+
+        let gap = empty
+            .iter_mut()
+            .enumerate()
+            .skip(file_len)
+            .filter_map(|(gap_len, gaps)| Some((*gaps.first()?, gap_len, gaps)))
+            .filter(|&(gap_pos, _, _)| gap_pos < file_pos)
+            .min();
+
+        match gap {
+            Some((gap_pos, gap_len, gaps)) => {
+                files.remove(&f);
+                gaps.pop_first();
+                files.insert((gap_pos, file_len, id));
+                let new_len = gap_len - file_len;
+                if new_len > 0 {
+                    empty[gap_len - file_len].insert(gap_pos + file_len);
+                }
+            }
+            None => {
+                res += id * (2 * file_pos + file_len - 1) * file_len / 2;
+                files.pop_last();
+            }
+        }
+    }
+    res
 }
 
-pub const SOLUTION: Day<usize, u64> = Day {
+fn p2(filename: &str) -> usize {
+    p2_(&read_lines(filename).next().unwrap())
+}
+
+pub const SOLUTION: Day<usize, usize> = Day {
     part_1: Task {
         examples: &["./inputs/day_09/example_1.txt", "./inputs/day_09/example_2.txt"],
         task: "./inputs/day_09/task.txt",
         func: p1,
     },
     part_2: Task {
-        examples: &["./inputs/day_09/example_1.txt"],
+        examples: &["./inputs/day_09/example_2.txt", "./inputs/day_09/example_3.txt"],
         task: "./inputs/day_09/task.txt",
         func: p2,
     },
@@ -60,7 +107,12 @@ pub const SOLUTION: Day<usize, u64> = Day {
 
 #[cfg(test)]
 mod d09_tests {
+    use std::hint::black_box;
+
     use super::*;
+
+    extern crate test;
+    use test::bench::Bencher;
 
     #[test]
     fn p1_example_test() {
@@ -72,7 +124,19 @@ mod d09_tests {
 
     #[test]
     fn p2_example_test() {
+        // 6408966547049 yay!
         let res = SOLUTION.part_2.run_example(0);
-        assert_eq!(res, 0);
+        assert_eq!(res, 2858);
+
+        let res = SOLUTION.part_2.run_example(1);
+        assert_eq!(res, 2900);
+    }
+
+    #[bench]
+    fn p2_1(b: &mut Bencher) {
+        let line = read_lines("./inputs/day_09/task.txt").next().unwrap();
+        //2645688
+
+        b.iter(|| black_box(p2_(&line)));
     }
 }
