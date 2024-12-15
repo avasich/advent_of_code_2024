d2c364958a662350c7aade7727759857eedac5ce
Artyom Vasich <vasich.dev@gmail.com>
2024-12-16 00:48:37 +0400

    day 12 p2

--- src/days/d12_garden_groups.rs
+++ src/days/d12_garden_groups.rs
@@ -15,7 +15,7 @@ fn extract_area(
 ) -> Option<Vec<(usize, Intervals)>> {
     let c = map[y0][x0]?;
     let (w, h) = (map[0].len(), map.len());
-    
+
     let mut res: HashMap<_, Vec<_>> = HashMap::new();
     let mut q = VecDeque::new();
     q.push_back((x0, y0));
@@ -77,11 +77,11 @@ fn intersection_length(a: &[(usize, usize)], b: &[(usize, usize)]) -> usize {
         let (start_a, end_a) = a[i];
         let (start_b, end_b) = b[j];
 
-        let start_overlap = start_a.max(start_b);
-        let end_overlap = end_a.min(end_b);
+        let start = start_a.max(start_b);
+        let end = end_a.min(end_b);
 
-        if start_overlap <= end_overlap {
-            res += end_overlap - start_overlap + 1;
+        if start <= end {
+            res += end - start + 1;
         }
 
         match end_a < end_b {
@@ -93,19 +93,54 @@ fn intersection_length(a: &[(usize, usize)], b: &[(usize, usize)]) -> usize {
     res
 }
 
-fn parse_intervals(filename: &str) -> impl Iterator<Item = Vec<(usize, Intervals)>> {
+fn sides_change(a: &[(usize, usize)], b: &[(usize, usize)]) -> usize {
+    let (mut i, mut j) = (0, 0);
+    let mut res = 0;
+
+    while i < a.len() && j < b.len() {
+        let (start_a, end_a) = a[i];
+        let (start_b, end_b) = b[j];
+
+        use std::cmp::Ordering;
+        match (start_a.cmp(&start_b), end_a.cmp(&end_b)) {
+            (Ordering::Equal, Ordering::Equal) => {
+                // ...aaaaaaa...
+                // ...bbbbbbb...
+                res += 4;
+            }
+            (Ordering::Equal, _) | (_, Ordering::Equal) => {
+                //  ..aaaaaa.......  ..aaaaaaaaaaa..
+                //  ..bbbbbbbbbbb..  ..bbbbbb.......
+
+                //  .......aaaaaa..   ..aaaaaaaaaaa..
+                //  ..bbbbbbbbbbb..   .......bbbbbb..
+                res += 2;
+            }
+            _ => {}
+        }
+
+        match end_a < end_b {
+            true => i += 1,
+            false => j += 1,
+        }
+    }
+
+    res
+}
+
+fn parse_intervals(filename: &str) -> impl Iterator<Item = (char, Vec<(usize, Intervals)>)> {
     let mut map: Vec<Vec<_>> =
         read_lines(filename).map(|line| line.chars().map(Some).collect()).collect();
     let (w, h) = (map[0].len(), map.len());
 
     (0..h)
         .flat_map(move |y| (0..w).map(move |x| (x, y)))
-        .flat_map(move |(x, y)| extract_area((x, y), &mut map))
+        .flat_map(move |(x, y)| Some((map[y][x]?, extract_area((x, y), &mut map)?)))
 }
 
 fn p1(filename: &str) -> usize {
     parse_intervals(filename)
-        .map(|rows| {
+        .map(|(_, rows)| {
             let (area, perimeter) = rows
                 .iter()
                 .flat_map(|(_, intervals)| intervals)
@@ -127,21 +162,22 @@ fn p1(filename: &str) -> usize {
         .sum()
 }
 
-/*
-
-0123456789
-aaa.aaaaa.
-..aaa.....
-aaa.aaaaaa.
-
-(0 8)
-(2 4)
-(0 2) (4 9)
-
- */
-
 fn p2(filename: &str) -> usize {
-    0
+    parse_intervals(filename)
+        .map(|(_, rows)| {
+            let (area, sides) = rows
+                .iter()
+                .flat_map(|(_, intervals)| intervals)
+                .map(|&(start, end)| (end - start + 1, 4))
+                .fold((0, 0), |(a1, p1), (a2, p2)| (a1 + a2, p1 + p2));
+
+            let diff =
+                rows.array_windows().map(|[(_, r1), (_, r2)]| sides_change(r1, r2)).sum::<usize>();
+
+            let total_sides = sides - diff;
+            total_sides * area
+        })
+        .sum()
 }
 
 pub const SOLUTION: Day<usize, usize> = day! { 12,
@@ -170,90 +206,21 @@ mod d12_tests {
 
     #[test]
     fn p1_example_test() {
-        let res = SOLUTION.part_1.run_example(0);
-        assert_eq!(res, 140);
-        let res = SOLUTION.part_1.run_example(1);
-        assert_eq!(res, 772);
-        let res = SOLUTION.part_1.run_example(2);
-        assert_eq!(res, 1930);
+        assert_eq!(SOLUTION.part_1.run_example(0), 140);
+        assert_eq!(SOLUTION.part_1.run_example(1), 772);
+        assert_eq!(SOLUTION.part_1.run_example(2), 1930);
     }
 
     #[test]
     fn p2_example_test() {
-        let res = SOLUTION.part_2.run_example(0);
-        assert_eq!(res, 80);
-        let res = SOLUTION.part_2.run_example(1);
-        assert_eq!(res, 436);
-        let res = SOLUTION.part_2.run_example(2);
-        assert_eq!(res, 236);
-        let res = SOLUTION.part_2.run_example(2);
-        assert_eq!(res, 368);
+        assert_eq!(SOLUTION.part_2.run_example(0), 80);
+        assert_eq!(SOLUTION.part_2.run_example(1), 436);
+        assert_eq!(SOLUTION.part_2.run_example(2), 236);
+        assert_eq!(SOLUTION.part_2.run_example(3), 368);
     }
 
     #[test]
     fn playground() {
-        p1(SOLUTION.part_1.examples[1]);
+        p2(SOLUTION.part_2.examples[1]);
     }
 }
-/*
-
-   0123
- 0 AAAA
- 1 BBCD
- 2 BBCC
- 3 EEEC
-
-
-'A': [(0, 0), (1, 0), (2, 0), (3, 0)],
-'B': [(0, 1), (1, 1), (0, 2), (1, 2)],
-'C': [(2, 1), (2, 2), (3, 2), (3, 3)],
-'D': [(3, 1)]
-'E': [(0, 3), (1, 3), (2, 3)],
-
-
-OOOOO
-OOOXO
-OOOOO
-OXOXO
-OOOOO
-
-
-'O': [
-    (0, 0), (1, 0), (2, 0), (3, 0), (4, 0)
-    (0, 1), (1, 1), (2, 1),         (4, 1),
-    (0, 2), (1, 2), (2, 2), (3, 2), (4, 2)
-    (0, 3),
-    (0, 4),
-    (2, 3), (1, 4), (2, 4), (4, 3), (3, 4), (4, 4)
-    ],
-'X': [(3, 3)]
-
-0: (0 4)
-1: (0 2) (4 4)
-
-(0, (0 4))
-(1, (0 2))
-(1, (4 4))
-
-
-
-
-(0, (2 3))
-
-
-(1, (4 4))
-
-
-
-{
-'X': [(3, 3)],
-
-'O': [
-(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
-(0, 1), (2, 1), (4, 1),
-(0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
-(0, 3), (2, 3), (4, 3),
-(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]}
-
-
- */
