0448fc66ac73b81103a2c6104a9886c8ea830349
Artyom Vasich <vasich.dev@gmail.com>
2024-12-17 18:13:56 +0400

    day 13 p2

--- src/days/d13_claw_contraption.rs
+++ src/days/d13_claw_contraption.rs
@@ -3,7 +3,7 @@ use crate::{
     utils::{Day, Task, read_lines},
 };
 
-type Point = (u64, u64);
+type Point = (i64, i64);
 
 fn parse_file(filename: &str) -> impl Iterator<Item = (Point, Point, Point)> {
     fn parse_xy(s: &str) -> Option<Point> {
@@ -19,30 +19,45 @@ fn parse_file(filename: &str) -> impl Iterator<Item = (Point, Point, Point)> {
         .flat_map(|[a, b, p]| Some((parse_xy(&a)?, parse_xy(&b)?, parse_xy(&p)?)))
 }
 
-fn p1(filename: &str) -> u64 {
+fn intersect((ax, ay): Point, (bx, by): Point, (px, py): Point) -> Option<Point> {
+    use num::integer::div_rem;
+    // i = (by * px - bx * py) / (ax * by - ay * bx)
+    // j = (ax * py - ay * px) / (ax * by - ay * bx)
+
+    match ax * by - ay * bx {
+        0 => None,
+        det => {
+            let i = match div_rem(by * px - bx * py, det) {
+                (i @ 0.., 0) => i,
+                _ => return None,
+            };
+            let j = match div_rem(ax * py - ay * px, det) {
+                (j @ 0.., 0) => j,
+                _ => return None,
+            };
+            Some((i, j))
+        }
+    }
+}
+
+fn price(filename: &str, add: i64) -> i64 {
     parse_file(filename)
-        .flat_map(|((ax, ay), (bx, by), (px, py))| {
-            (0..)
-                .map(move |i| (i, ax * i, ay * i))
-                .take_while(move |&(_, x, y)| x <= px && y <= py)
-                .filter_map(move |(i, x, y)| {
-                    let (div_x, rem_x) = ((px - x) / bx, (px - x) % bx);
-                    let (div_y, rem_y) = ((py - y) / by, (py - y) % by);
-                    (rem_x == 0 && rem_y == 0 && div_x == div_y).then_some((i, div_x))
-                })
-                .map(|(an, bn)| 3 * an + bn)
-                .min()
-        })
+        .flat_map(|(a, b, (px, py))| intersect(a, b, (px + add, py + add)))
+        .map(|(i, j)| 3 * i + j)
         .sum()
 }
-fn p2(filename: &str) -> u64 {
-    const ADD: u64 = 10000000000000;
-    let _ = parse_file(filename).map(|(a, b, (px, py))| (a, b, (px + ADD, py + ADD)));
-    0
+
+fn p1(filename: &str) -> i64 {
+    price(filename, 0)
 }
-pub const SOLUTION: Day<u64, u64> = day! { 13,
+
+fn p2(filename: &str) -> i64 {
+    price(filename, 10000000000000)
+}
+
+pub const SOLUTION: Day<i64, i64> = day! { 13,
     part_1: { examples: ["example.txt"], func: p1 },
-    part_2: { examples: ["example.txt"], func: p2 }
+    part_2: { examples: [], func: p2 }
 };
 
 #[cfg(test)]
@@ -52,10 +67,5 @@ mod d13_tests {
     #[test]
     fn p1_example_test() {
         assert_eq!(SOLUTION.part_1.run_example(0), 480);
-    }
-
-    #[test]
-    fn p2_example_test() {
-        assert_eq!(SOLUTION.part_2.run_example(0), 0);
     }
 }
